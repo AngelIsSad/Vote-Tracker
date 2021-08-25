0c0ae06e8ba10457f58db22381af0a5840ab5f59
TK-Angel <72151217+TK-Angel@users.noreply.github.com>
2021-08-25 16:01:30 +0200

    master

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ const Client = require('./Structure/Client');
 const client = new Client({ prefix: '!', channelToSend: '875064682174619696', topggBotId: '835525646292877323', roleId: '875064681109262387' });
 
 // Bot Token, MongoURi, Topgg Auth Token
-return client.start('ODM2NTQ5MTE1MjE0MTY4MDc0.YIfnEw.C2TMZZlS8XdevyW_pnIsb035Fwo', 'mongodb+srv://tk_angel:alikhan110@cluster0.h5adc.mongodb.net/modbotdev?retryWrites=true&w=majority', 'piss')
+return client.start('ODM2NTQ5MTE1MjE0MTY4MDc0.YIfnEw.bFYQcpmFVLXftELoTO_K9QxLDHY', 'mongodb+srv://tk_angel:alikhan110@cluster0.h5adc.mongodb.net/modbotdev?retryWrites=true&w=majority', 'piss')
