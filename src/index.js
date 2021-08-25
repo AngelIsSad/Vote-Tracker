a8094c78ea4e63d528bdc2b1235c0f4526f6852f
TK-Angel <72151217+TK-Angel@users.noreply.github.com>
2021-08-25 15:38:58 +0200

    master

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const Client = require('./Structure/Client');
-const client = new Client({ prefix: '!', channelToSend: '875064682174619696', topggBotId: '835525646292877323', roleId: '880072030660218930' });
+const client = new Client({ prefix: '!', channelToSend: '875064682174619696', topggBotId: '835525646292877323', roleId: '875064681109262387' });
 
 // Bot Token, MongoURi, Topgg Auth Token
 return client.start('ODM2NTQ5MTE1MjE0MTY4MDc0.YIfnEw.C2TMZZlS8XdevyW_pnIsb035Fwo', 'mongodb+srv://tk_angel:alikhan110@cluster0.h5adc.mongodb.net/modbotdev?retryWrites=true&w=majority', 'piss')
