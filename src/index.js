11e646eaa071cf3cf7b5c135936f18542dcb1466
Angel <72151217+AngelIsSad@users.noreply.github.com>
2021-10-24 12:11:40 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const Client = require('./Structure/Client');
-const client = new Client({ prefix: '!', channelToSend: 'VOTE-LOG-CHANNELID', topggBotId: 'YOUR-BOT-ID', roleId: 'ROLEID-YOU-WANT-TO-GIVE' });
+const client = new Client({ prefix: 'YOUR-BOT-PREFIX', channelToSend: 'VOTE-LOG-CHANNELID', topggBotId: 'YOUR-BOT-ID', roleId: 'ROLEID-YOU-WANT-TO-GIVE' });
 
 // Bot Token, MongoURi, Topgg Auth Token
 return client.start('BOT-TOKEN-GOES-HERE', 'MONGO-DB-URI-GOES-HERE', 'AUTHORIZATION-KEY')
